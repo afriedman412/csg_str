9d7940bd63a209b4213e9b0cc0cc85ec73be9c35
Andy Friedman <afriedman412@gmail.com>
2025-11-28 23:38:45 -0500

    added / endpoint

--- app/main.py
+++ app/main.py
@@ -36,5 +36,10 @@ app.include_router(api.router)
 templates = Jinja2Templates(directory="templates")
 
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
